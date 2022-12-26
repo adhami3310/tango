9b6fbbc0819013b695784db3205c82128c817c3b
Adhami3310 <khaleel.aladhami@gmail.com>
2022-12-26 11:14:54 -0600

    added count operations to tango

--- main.cc
+++ main.cc
@@ -1,18 +1,18 @@
 #include <iostream>
-
-#include "src/tango.hpp"
+#include <stdlib.h>
+#include "src/tangocount.hpp"
 using namespace std;
 
 int main(int argc, char **argv) {
-    for (int N = 1000000; N <= 1000000; N++) {
+    for (int N = 5000; N <= 1000000; N = min(int(N*1.25), N+50000)) {
         auto tango = TangoTree<int, int>();
         for (int i = 0; i < N; i++) {
             tango.insert(i, i);
         }
         tango.lock();
-        for (int i = 0; i < 25*N; i++) {
-            tango.find(i%N)->val;
+        for (int i = 0; i < N; i++) {
+            tango.find(rand()%(N))->val;
         }
-        tango.checkIntegrity();
+        cout << N << "," << tango.operations << endl;
     }
 }
